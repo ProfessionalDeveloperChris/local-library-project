f4d4c6b821306471ac89eb91570d006581298c3c
Christopher Garcia <cgarcia0566@gmail.com>
2024-11-10 21:19:50 -0800

    Update main.test.js

--- test/main.test.js
+++ test/main.test.js
@@ -1,104 +1,48 @@
-const { expect } = require("chai");
-const {
-  getTotalBooksCount,
-  getTotalAccountsCount,
-  getBooksBorrowedCount,
-  getMostCommonGenres,
-  getMostPopularBooks,
-  getMostPopularAuthors,
-} = require("../public/src/home.js");
+function getTotalBooksCount(books) {
+    return books.length;
+}
 
-const authorsFixture = require("./fixtures/authors.fixture");
-const booksFixture = require("./fixtures/books.fixture");
+function getTotalAccountsCount(accounts) {
+    return accounts.length;
+}
 
-describe("Home Page", () => {
-  let authors;
-  let books;
+function getBooksBorrowedCount(books) {
+    return books.filter(book => !book.borrows[0].returned).length;
+}
 
-  beforeEach(() => {
-    authors = authorsFixture.slice();
-    books = booksFixture.slice();
-  });
+function getMostCommonGenres(books) {
+    const genreCounts = books.reduce((acc, book) => {
+        acc[book.genre] = (acc[book.genre] || 0) + 1;
+        return acc;
+    }, {});
+    return Object.entries(genreCounts)
+        .map(([name, count]) => ({ name, count }))
+        .sort((a, b) => b.count - a.count)
+        .slice(0, 5);
+}
 
-  describe("getTotalBooksCount()", () => {
-    it("should return the total number of books in the array", () => {
-      const actual = getTotalBooksCount([{}, {}]);
-      expect(actual).to.equal(2);
+function getMostPopularBooks(books) {
+    return books.map(book => ({
+        name: book.title,
+        count: book.borrows.length
+    })).sort((a, b) => b.count - a.count)
+      .slice(0, 5);
+}
+
+function getMostPopularAuthors(books, authors) {
+    const authorBorrowCounts = authors.map(author => {
+        const authorBooks = books.filter(book => book.authorId === author.id);
+        const borrowCount = authorBooks.reduce((acc, book) => acc + book.borrows.length, 0);
+        return { name: `${author.name.first} ${author.name.last}`, count: borrowCount };
     });
+    return authorBorrowCounts.sort((a, b) => b.count - a.count).slice(0, 5);
+}
 
-    it("should return zero if the array is empty", () => {
-      const actual = getTotalBooksCount([]);
-      expect(actual).to.equal(0);
-    });
-  });
-
-  describe("getTotalAccountsCount()", () => {
-    it("should return the total number of accounts in the array", () => {
-      const actual = getTotalAccountsCount([{}, {}]);
-      expect(actual).to.equal(2);
-    });
-
-    it("should return zero if the array is empty", () => {
-      const actual = getTotalAccountsCount([]);
-      expect(actual).to.equal(0);
-    });
-  });
-
-  describe("getBooksBorrowedCount()", () => {
-    it("should return the total number of books that are currently borrowed", () => {
-      const actual = getBooksBorrowedCount(books);
-      expect(actual).to.equal(6);
-    });
-  });
-
-  describe("getMostCommonGenres()", () => {
-    it("should return an ordered list of most common genres", () => {
-      const actual = getMostCommonGenres(books);
-      const [first, second] = [
-        { name: "Science", count: 3 },
-        { name: "Classics", count: 2 },
-      ];
-      expect(actual[0]).to.eql(first);
-      expect(actual[1]).to.eql(second);
-    });
-
-    it("should limit the list to the top five", () => {
-      const actual = getMostCommonGenres(books);
-      expect(actual.length).to.equal(5);
-    });
-  });
-
-  describe("getMostPopularBooks()", () => {
-    it("should return an ordered list of most popular books", () => {
-      const actual = getMostPopularBooks(books);
-      const [first, second] = [
-        { name: "sit eiusmod occaecat eu magna", count: 11 },
-        { name: "ullamco est minim", count: 5 },
-      ];
-      expect(actual[0]).to.eql(first);
-      expect(actual[1]).to.eql(second);
-    });
-
-    it("should limit the list to the top five", () => {
-      const actual = getMostPopularBooks(books);
-      expect(actual.length).to.equal(5);
-    });
-  });
-
-  describe("getMostPopularAuthors()", () => {
-    it("should return an ordered list of most popular authors", () => {
-      const actual = getMostPopularAuthors(books, authors);
-      const [first, second] = [
-        { name: "Susanne Lawson", count: 11 },
-        { name: "Matthews Sanders", count: 5 },
-      ];
-      expect(actual[0]).to.eql(first);
-      expect(actual[1]).to.eql(second);
-    });
-
-    it("should limit the list to the top five", () => {
-      const actual = getMostPopularAuthors(books, authors);
-      expect(actual.length).to.equal(5);
-    });
-  });
-});
+module.exports = {
+    getTotalBooksCount,
+    getTotalAccountsCount,
+    getBooksBorrowedCount,
+    getMostCommonGenres,
+    getMostPopularBooks,
+    getMostPopularAuthors,
+};
